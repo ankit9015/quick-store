4aa93bc5270f88b7d6c66c70685a9d2dfa498d72
Ankit Joshi <ankitoshi9015@gmail.com>
2025-03-23 01:42:40 +0530

    feat (productScreen): add style theme and fonts

--- src/app/screens/products/ProductInfoCard.js
+++ src/app/screens/products/ProductInfoCard.js
@@ -1,32 +1,95 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import styled from "styled-components/native";
+import { Text, Image, View } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
 
-export const ProductInfoCard = ({ restaurant = {} }) => {
+
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+
+const ProductCard = styled(Card)`
+	background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const ProductCardCover = styled(Card.Cover)`
+	padding: ${(props) => props.theme.space[3]};
+	background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Address = styled(Text)`
+	font-family: ${(props) => props.theme.fonts.body};
+	font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Title = styled(Text)`
+	font-family: ${(props) => props.theme.fonts.heading};
+	font-size: ${(props) => props.theme.fontSizes.body};
+	color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+	padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+	flex-direction: row;
+	padding-top: ${(props) => props.theme.space[2]};
+	padding-bottom: ${(props) => props.theme.space[2]};
+`;
+const Section = styled.View`
+	flex-direction: row;
+	align-items: center;
+`;
+const SectionEnd = styled.View`
+	flex: 1;
+	flex-direction: row;
+	justify-content: flex-end;
+`;
+
+export const ProductInfoCard = ({ Product = {} }) => {
 	const {
 		name = "Product A",
-		icon,
+		icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
 		photos = ["https://picsum.photos/200/300"],
 		address = "some address",
 		isOpenNow = true,
 		rating = 4,
-		isClosedTemporarily,
-	} = restaurant;
+		isClosedTemporarily = true,
+	} = Product;
+
+	const ratingArray = Array.from(new Array(Math.floor(rating)));
 
 	return (
-		<Card elevation={5} style={styles.card}>
-			<Card.Cover
-				key={name}
-				style={styles.cover}
-				source={{ uri: photos[0] }}
-			/>
-			<Text style={styles.title}>{name}</Text>
-		</Card>
+		<ProductCard elevation={5}>
+			<ProductCardCover key={name} source={{ uri: photos[0] }} />
+			<Info>
+				<Title>{name}</Title>
+				<Section>
+					<Rating>
+						{ratingArray.map((_, i) => (
+							<SvgXml key={i} xml={star} width={20} height={20} />
+						))}
+					</Rating>
+					<SectionEnd>
+						{isClosedTemporarily && (
+							<Text variant="label" style={{ color: "red" }}>
+								CLOSED TEMPORARILY
+							</Text>
+						)}
+						<View style={{ paddingLeft: 16 }} />
+						{isOpenNow && (
+							<SvgXml xml={open} width={20} height={20} />
+						)}
+						<View style={{ paddingLeft: 16 }} />
+						<Image
+							style={{ width: 15, height: 15 }}
+							source={{ uri: icon }}
+						/>
+					</SectionEnd>
+				</Section>
+				<Address>{address}</Address>
+			</Info>
+		</ProductCard>
 	);
 };
-
-const styles = StyleSheet.create({
-	card: { backgroundColor: "white" },
-	cover: { padding: 20, backgroundColor: "white" },
-	title: { padding: 16 },
-});
--- src/app/screens/products/ProductsScreen.js
+++ src/app/screens/products/ProductsScreen.js
@@ -1,30 +1,30 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, SafeAreaView, View } from "react-native";
+import { StatusBar, SafeAreaView } from "react-native";
+import styled from "styled-components/native";
 import { ProductInfoCard } from "./ProductInfoCard";
 
-export const ProductsScreen = () => (
-	<SafeAreaView style={styles.container}>
-		<View style={styles.search}>
-			<Searchbar />
-		</View>
-		<View style={styles.list}>
-			<ProductInfoCard />
-		</View>
-	</SafeAreaView>
-);
+const SafeArea = styled(SafeAreaView)`
+	flex: 1;
+	${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		marginTop: StatusBar.currentHeight,
-	},
-	search: {
-		padding: 16,
-	},
-	list: {
-		flex: 1,
-		padding: 16,
-		backgroundColor: "blue",
-	},
-});
+const SearchContainer = styled.View`
+	padding: ${(props) => props.theme.space[3]};
+`;
+
+const ProductListContainer = styled.View`
+	flex: 1;
+	padding: ${(props) => props.theme.space[3]};
+`;
+
+export const ProductsScreen = () => (
+	<SafeArea>
+		<SearchContainer>
+			<Searchbar />
+		</SearchContainer>
+		<ProductListContainer>
+			<ProductInfoCard />
+		</ProductListContainer>
+	</SafeArea>
+);
