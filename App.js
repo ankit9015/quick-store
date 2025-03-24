e39664e8cf11ad2a1de74c1eebdf0bcfc2d50ba2
Ankit Joshi <ankitoshi9015@gmail.com>
2025-03-25 01:57:23 +0530

    feat: add tab navigation

--- App.js
+++ App.js
@@ -9,6 +9,39 @@ import {
 	Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SafeArea } from "./src/app/components/safeArea";
+import { Text } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () => (
+	<SafeArea>
+		<Text>Settings</Text>
+	</SafeArea>
+);
+const Map = () => (
+	<SafeArea>
+		<Text>Map</Text>
+	</SafeArea>
+);
+
+const TabBarIcon = ({ color, size, route }) => {
+	let iconName;
+
+	if (route.name === "Products") {
+		iconName = "list";
+	} else if (route.name === "Settings") {
+		iconName = "settings";
+	} else if (route.name === "Map") {
+		iconName = "map";
+	}
+
+	// You can return any component that you like here!
+	return <Ionicons name={iconName} size={size} color={color} />;
+};
 
 export default function App() {
 	const [oswaldLoaded] = useOswald({
@@ -26,7 +59,25 @@ export default function App() {
 	return (
 		<>
 			<ThemeProvider theme={theme}>
-				<ProductsScreen />
+				<NavigationContainer>
+					<Tab.Navigator
+						screenOptions={({ route }) => ({
+							tabBarIcon: (props) =>
+								TabBarIcon({ ...props, route }),
+						})}
+						tabBarOptions={{
+							activeTintColor: "red",
+							inactiveTintColor: "gray",
+						}}
+					>
+						<Tab.Screen
+							name='Products'
+							component={ProductsScreen}
+						/>
+						<Tab.Screen name='Map' component={Map} />
+						<Tab.Screen name='Settings' component={Settings} />
+					</Tab.Navigator>
+				</NavigationContainer>
 			</ThemeProvider>
 			<ExpoStatusBar style='auto' />
 		</>
--- src/app/screens/products/ProductsScreen.js
+++ src/app/screens/products/ProductsScreen.js
@@ -1,30 +1,50 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, SafeAreaView } from "react-native";
 import styled from "styled-components/native";
 import { ProductInfoCard } from "./productInfoCard/ProductInfoCard";
+import { Spacer } from "../../components/spacer";
+import { SafeArea } from "../../components/safeArea";
 
-const SafeArea = styled(SafeAreaView)`
-	flex: 1;
-	${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
 
 const SearchContainer = styled.View`
 	padding: ${(props) => props.theme.space[3]};
 `;
 
-const ProductListContainer = styled.View`
-	flex: 1;
-	padding: ${(props) => props.theme.space[3]};
-`;
+const ProductList = styled.FlatList.attrs({
+	contentContainerStyle: {
+		padding: 16,
+	},
+})``;
 
 export const ProductsScreen = () => (
 	<SafeArea>
 		<SearchContainer>
 			<Searchbar />
 		</SearchContainer>
-		<ProductListContainer>
-			<ProductInfoCard />
-		</ProductListContainer>
+
+		<ProductList
+			data={[
+				{ name: 1 },
+				{ name: 2 },
+				{ name: 3 },
+				{ name: 4 },
+				{ name: 5 },
+				{ name: 6 },
+				{ name: 7 },
+				{ name: 8 },
+				{ name: 9 },
+				{ name: 10 },
+				{ name: 11 },
+				{ name: 12 },
+				{ name: 13 },
+				{ name: 14 },
+			]}
+			renderItem={() => (
+				<Spacer position='bottom' size='large'>
+					<ProductInfoCard />
+				</Spacer>
+			)}
+			keyExtractor={(item) => item.name}
+		/>
 	</SafeArea>
 );
